fa3a2708bcd31fbbc280117fdb084db5ce881fb6
Kim baekjung <zvgandam@gmail.com>
2025-01-27 00:14:48 +0900

    edit vercel deploy

--- webpack.config.js
+++ webpack.config.js
@@ -2,40 +2,41 @@ const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.ts',
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.ts', '.js'],
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/dist/'
-    },
-    devServer: {
-        static: {
-            directory: path.join(__dirname, '/'),
-        },
-        compress: true,
-        port: 9000,
-        hot: true
-    },
-    plugins: [
-        new CopyPlugin({
-            patterns: [
-                { from: 'index.html', to: '.' },
-                { from: 'assets', to: 'assets' },
-                { from: 'sitemap.xml', to: '.' },
-            ],
-        }),
+  mode: 'development',
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
     ],
-}; 
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, '/'),
+    },
+    compress: true,
+    port: 9000,
+    hot: true,
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'index.html', to: '.' },
+        { from: 'assets', to: 'assets' },
+        { from: 'sitemap.xml', to: '.' },
+        { from: 'src/styles.css', to: 'styles.css' },
+      ],
+    }),
+  ],
+};
